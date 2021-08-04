2da6e6905d032e28f1c9db5f29a5df1b11651e95
Felipe Tófoli <feadt@msn.com>
2021-08-04 01:12:23 -0300

    Adding poorly formatted code.

--- src/Sandbox.WebAPI/App_Start/WebApiConfig.cs
+++ src/Sandbox.WebAPI/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace Sandbox.WebAPI
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
